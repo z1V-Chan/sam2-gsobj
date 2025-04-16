e5282a04b83ec217225ad3451a31aedfabe9660d
z1V-Chan <zivchan832@gmail.com>
2025-04-17 04:28:36 +0800

    requirement modified

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 # Required dependencies
 REQUIRED_PACKAGES = [
-    "torch==2.3.1",
-    "torchvision==0.18.1",
-    "numpy==1.26.4",
+    "torch>=2.3.1",
+    "torchvision>=0.18.1",
+    "numpy>=1.26.4",
     "hydra-core>=1.3.2",
     "iopath>=0.1.10",
 ]
